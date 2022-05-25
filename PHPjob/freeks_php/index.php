411b233a3499390db57c48d690005c1bf516280b
Leotamn <103520627+Leotamn@users.noreply.github.com>
2022-05-26 02:36:31 +0900

    変数とデータ型
    やった

--- PHPjob/freeks_php/index.php
+++ PHPjob/freeks_php/index.php
@@ -1,5 +1,24 @@
 <?php
-    $numbers = {123, 200, 300};
-    echo $numbers;
+$message = "Hello World!";
+var_dump($message);
+
+$x=8;
+$y=5.55;
+echo '<br>';
+var_dump($x);
+echo '<br>';
+var_dump($y);
+
+$flag = true;
+var_dump($flag);
+
+$emp = null;
+var_dump($emp);
+
+$en_words = 'Hello World';
+var_dump($en_words);
+echo '<br>';
+$ja_words = 'こんにちは';
+var_dump($ja_words);
 
 ?>
