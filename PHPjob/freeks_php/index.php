10a6c728a1749cdbc19fcb36beb478de73e768df
Leotamn <103520627+Leotamn@users.noreply.github.com>
2022-06-03 00:12:55 +0900

    条件分岐
    チャレンジ問題完了

--- PHPjob/freeks_php/index.php
+++ PHPjob/freeks_php/index.php
@@ -2,10 +2,14 @@
    $name = 'taro';
    $password = 'pass';
 
-   if (1 === $name) {
-       echo 'パスワードが間違っています';
+   if (1 === $name and $password) {
+       echo 'ログイン成功です';
+   } elseif (1 === $name) {
+       echo 'パスワードが間違っています。';
+   } elseif (1 === $password) {
+       echo '名前が間違っています。';
    } else {
-       echo '名前が間違っています';
+       echo '入力情報が間違っています。';
    }
 
 
