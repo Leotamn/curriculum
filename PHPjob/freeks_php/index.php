4659a6ec5b98354aa39f3a5f592958490927373d
Leotamn <103520627+Leotamn@users.noreply.github.com>
2022-06-06 23:04:07 +0900

    やってみた
    ぴー

--- PHPjob/freeks_php/index.php
+++ PHPjob/freeks_php/index.php
@@ -1,16 +1,11 @@
 <?php
-   $name = 'taro';
-   $password = 'pass';
+   $num = 0;
 
-   if (1 === $name and $password) {
-       echo 'ログイン成功です';
-   } elseif (1 === $name) {
-       echo 'パスワードが間違っています。';
-   } elseif (1 === $password) {
-       echo '名前が間違っています。';
-   } else {
-       echo '入力情報が間違っています。';
+   while($num < 300) {
+       echo $num;
+       $num++;
+       if($num == 101) {
+           break;
+       }
    }
-
-
 ?>
