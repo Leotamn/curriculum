1be204a20cc28a659be3633ecf3624be34d6390f
Leotamn <103520627+Leotamn@users.noreply.github.com>
2022-06-14 03:06:39 +0900

    連想配列
    しんだ

--- PHPjob/freeks_php/index.php
+++ PHPjob/freeks_php/index.php
@@ -1,5 +1,25 @@
 <?php
-   $countries = ["America", "Japan", "China", "Korea"];
-   var_dump($countries);
+   $fruits = ["apple" => "りんご", "orange" => "みかん", "grape" => "ぶどう"];
+   $fruits = ["peach" => "もも" ];
+
+   // りんごを出力
+   echo $fruits["apple"];
+   // みかんを出力
+   echo $fruits["orange"];
+   // ぶどうを出力
+   echo $fruits["grape"];
+   // ももを出力
+   echo $fruits["peach"];
+
+
+
+   array(4) {
+      ["apple"] => string(9) "りんご"
+      ["orange"] => string(9) "みかん"
+      ["grape"] => string(9) "ぶどう"
+      ["peach"] => string(9) "もも"
+  }
    
+   var_dump($fruits);
+  
 ?>
