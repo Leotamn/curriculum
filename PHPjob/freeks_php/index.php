4ee84e11778faf9ab106b4be9c1a3f87e33699d9
Leotamn <103520627+Leotamn@users.noreply.github.com>
2022-06-16 02:25:15 +0900

    関数
    三角形の面積

--- PHPjob/freeks_php/index.php
+++ PHPjob/freeks_php/index.php
@@ -1,11 +1,15 @@
 <?php
-   $fruits = ["apple" => "といったらりんご","orange" => "といったらみかん","peach" => "といったらもも"];
+
    
-   foreach ($fruits as $key => $value) {
-      echo $key;
-      echo $value;
+   function getTraiangleArea($base , $height) {
+      $area = $base * $height / 2;
+      print "三角形の面積は" .$area."だよ";
       echo "<br>";
    }
 
+   getTraiangleArea(10,5);
+   getTraiangleArea(15,8);
+   getTraiangleArea(8,6);
+   
 
 ?>
