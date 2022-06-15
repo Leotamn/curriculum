07977f1e3dd11e3b28246447919602948f117de2
Leotamn <103520627+Leotamn@users.noreply.github.com>
2022-06-15 17:12:05 +0900

    echo
    けした

--- PHPjob/freeks_php/index.php
+++ PHPjob/freeks_php/index.php
@@ -2,15 +2,7 @@
    $fruits = ["apple" => "りんご", "orange" => "みかん", "grape" => "ぶどう"];
    $fruits = ["peach" => "もも" ];
 
-   // りんごを出力
-   echo $fruits["apple"];
-   // みかんを出力
-   echo $fruits["orange"];
-   // ぶどうを出力
-   echo $fruits["grape"];
-   // ももを出力
-   echo $fruits["peach"];
-
+  
 
 
    array(4) {
