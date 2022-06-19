807c5caec5618050d3119a56c13538887d2a9c5d
Leotamn <103520627+Leotamn@users.noreply.github.com>
2022-06-20 03:06:39 +0900

    修正
    ４の++の後に;ついき
    ５の書式修正

--- PHPjob/3-1/FizzBuzz.php
+++ PHPjob/3-1/FizzBuzz.php
@@ -1,8 +1,8 @@
 <?php
 $i=0
 
-  for ($i = 1; $i <= 100; $i++) {
-    if($i % 3 === 0) && ($i % 5 === 0) {
+  for ($i = 1; $i <= 100; $i++;) {
+    if($i % 3 === 0 && $i % 5 === 0) {
       echo 'FizzBuzz!!' . "<br>";
     } elseif ($i % 3 === 0) {
       echo 'Fizz!' . "<br>";
