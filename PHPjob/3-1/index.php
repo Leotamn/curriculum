ed6ad7fa98b96e53bf127a94752c84ddd1f4452b
Leotamn <103520627+Leotamn@users.noreply.github.com>
2022-06-23 23:39:44 +0900

    チャレンジ
    できた

--- PHPjob/3-1/index.php
+++ PHPjob/3-1/index.php
@@ -1,6 +1,8 @@
 <form action="result.php" method="post">
   名前：<input type="text" name="my_name" />
   <br>
+  メールアドレス:<input type="address" name="address" />
+  <br>
   パスワード：<input type="password" name="password" />
   <br>
   <input type="submit" value="送信" />
