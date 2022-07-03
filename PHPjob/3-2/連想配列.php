105b4950993592632f4ea6281c18607582a6ad0f
Leotamn <103520627+Leotamn@users.noreply.github.com>
2022-07-04 00:08:11 +0900

    連想配列
    修正

--- PHPjob/3-2/連想配列.php
+++ PHPjob/3-2/連想配列.php
@@ -1,5 +1,5 @@
 <?php
-$fruits = ["apple" => "りんご","orange" => "みかん","peach" => "もも"];
+/*$fruits = ["apple" => "りんご","orange" => "みかん","peach" => "もも"];
 
 /*echo $fruits[0];
 echo '<br>';
@@ -10,18 +10,15 @@ echo '<br>';
 echo $fruits[2];
 echo '<br>';*/
 
-$fruits = array(
-    'りんご' => '300',
-    'みかん' => '150',
-    'もも' => '3000',
-);
-
-
-foreach( $fruits as $key => $value) {
-    echo $key;
-    echo "は";
-    echo $value;
-    echo "円です。<br>";
-
+function name_price($name, $price)
+{
+  return("$name の値段は $price 円です。");
 }
+
+$ary1 = array("りんご", "みかん", "もも");
+$ary2 = array(300, 150, 3000);
+
+print_r(array_map("name_price", $ary1, $ary2));
+; 
+
 ?>
