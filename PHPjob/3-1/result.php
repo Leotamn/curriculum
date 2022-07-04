5b937833a2e1752e2944c345088ebd82f97a9c24
Leotamn <103520627+Leotamn@users.noreply.github.com>
2022-07-05 03:57:54 +0900

    select
    追加

--- PHPjob/3-1/result.php
+++ PHPjob/3-1/result.php
@@ -1,8 +1,12 @@
 <?php
 $my_name = $_POST['my_name'];
-$mailadress = $_POST['address'];
+$mailaddress = $_POST['address'];
 $password = $_POST['password'];
+$sex = $_POST['sex'];
+$hidden_param = $_POST['hidden_param'];
 ?>
 <p>私の名前は、<?php echo $my_name; ?></p>
 <p>メールアドレスは、<?php echo $address; ?></p>
 <p>私のパスワードは、<?php echo $password; ?></p>
+<p>性別は<?php echo $sex; ?>です。</p>
+<p>性別は<?php echo $hidden_param; ?>です。</p>
