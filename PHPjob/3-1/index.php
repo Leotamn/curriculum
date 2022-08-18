426c63fda5917660903bdeb7e965dc809eb86fba
Leotamn <103520627+Leotamn@users.noreply.github.com>
2022-08-18 23:46:36 +0900

    い
    修正

--- PHPjob/3-1/index.php
+++ PHPjob/3-1/index.php
@@ -1,24 +1,9 @@
 <form action="result.php" method="post">
-  名前：<input type="text" name="my_name" />
-  <br>
-  メールアドレス:<input type="text" name="address" />
-  <br>
-  パスワード：<input type="password" name="password" />
-  <br>
-  性別：
-  <input type="radio"name="sex" value="男性">男性
-  <input type="radio"name="sex" value="女性">女性
-  <br>
-  <input type="hidden" name="hidden_param" value="隠しパラメータから" />
-  <select name="number">
-    <?php for ($i=1;$i<=10;$i++){ ?>
-      <option value="<?php echo $i; ?>">
-         <?php echo $i; ?>
-      </option>
-      <?php } ?>
-    </select>
-  
-  </select>
-
-  <input type="submit" value="送信するよ"/>
+    お名前：<input type="text" name="my name" /><br>
+    ご希望商品：
+    <input type="radio" name="select" value="A賞">A賞
+    <input type="radio" name="select" value="B賞">B賞
+    <input type="radio" name="select" value="C賞">C賞<br>   
+    個数：<input type="select" name="個数" value=""><br>
+    <input type="submit" value="送信"/>
 </form>
