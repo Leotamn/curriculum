414cfa87aa79fe505fd855e68af3be19f891463c
Leotamn <103520627+Leotamn@users.noreply.github.com>
2022-08-18 23:47:17 +0900

    Update index.php
    ｒ

--- PHPjob/3-1/index.php
+++ PHPjob/3-1/index.php
@@ -4,6 +4,6 @@
     <input type="radio" name="select" value="A賞">A賞
     <input type="radio" name="select" value="B賞">B賞
     <input type="radio" name="select" value="C賞">C賞<br>   
-    個数：<input type="select" name="個数" value=""><br>
+    個数：<input type="select" name="number" ><br>
     <input type="submit" value="送信"/>
 </form>
