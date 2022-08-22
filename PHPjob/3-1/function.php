b5645de121e5260ad61a0185c13ca5661db4587b
Leotamn <103520627+Leotamn@users.noreply.github.com>
2022-08-22 18:45:21 +0900

    ちゃれんじ
    復習

--- PHPjob/3-1/function.php
+++ PHPjob/3-1/function.php
@@ -1,5 +1,29 @@
 <?php
+    $testFile = "test.txt";
+    $contents = "にこにこ";
 
-echo strtotime("last Sundady");
+    if (is_writable($testFile)) {
+        $fp = fopen($testFile, "w");
+        fwrite($fp, $contents);
+        fclose($fp);
+        echo "finish writing";
 
+    } else {
+        echo "not writable";
+        exit;
+    }
+
+
+    $test_file = "test2.txt";
+
+    if (is_readable($test_file)){
+        $fp = fopen($test_file,"r");
+        while ($line = fgets($fp)) {
+            echo $line.'<br>';
+        }
+        fclose($fp);
+    } else {
+        echo "not readable";
+        exit;
+    }
 ?>
