499cd511332783b2e49a9cd9dcafb4aebaed08e2
Leotamn <103520627+Leotamn@users.noreply.github.com>
2022-08-23 01:00:54 +0900

    チャレンジ

--- PHPjob/3-3/fortune.php
+++ PHPjob/3-3/fortune.php
@@ -1,4 +1,7 @@
-<?php
+<form acton = "result.php" method="post">
 
+    <input type="text" name="number">
+    <input type="submit" value="占う">
 
-?>
+    
+</form>
---
+++ PHPjob/3-3/result.php
@@ -0,0 +1,13 @@
+<?php
+
+$number = $POST[mt_rand(1, 10)];
+$date = echo "date(YYYY/m/dd)";
+
+<p><?php echo $date; ?>の運勢は</p>
+<p>選ばれた数字は<?php echo $number; ?></p>
+
+?>
+
+
+
+>
