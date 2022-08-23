37a594cc6d37e0e41953eda613a05d4f227b35ef
Leotamn <103520627+Leotamn@users.noreply.github.com>
2022-08-24 00:41:10 +0900

    追記

--- PHPjob/3-3/result.php
+++ PHPjob/3-3/result.php
@@ -1,10 +1,29 @@
 <?php
 
-$number = $POST[mt_rand(1, 10)];
-$date = echo "date(YYYY/m/dd)";
+$number = $POST['number'];
 
-<p><?php echo $date; ?>の運勢は</p>
+
+
+<p><?php echo date("Y-m-d"); ?>の運勢は</p>
 <p>選ばれた数字は<?php echo $number; ?></p>
+<p>$pref = 0
+switch ($pref){
+    case 0:
+        echo "凶";
+        break;
+    case 1,2,3:
+        echo "小吉";
+        break;
+    case 4,5,6:
+        echo "中吉";
+        break;
+    case 7,8:
+        echo "吉";
+        break;
+    case 9:
+        echo "大吉";
+        break;
+}</p>
 
 ?>
 
