93c286e562614b485e1a6b6b80bcfc423d8b50ff
Leotamn <103520627+Leotamn@users.noreply.github.com>
2022-09-11 23:32:29 +0900

    question
    追記forreachのみ

--- PHPjob/3-4/question.php
+++ PHPjob/3-4/question.php
@@ -1,19 +1,36 @@
 <?php
- echo $_POST["confirm"];
-
-input type="radio" name=""
-
-//② ①で作成した、配列から正解の選択肢の変数を作成してください
+ 
+ $my_name = $_POST['my_name'];
+ $number = $POST['number'];
+ $language = $POST['language'];
+ $command = $POST['command'];
+ 
 
 ?>
-<p>お疲れ様です<!--POST通信で送られてきた名前を出力-->さん</p>
-<!--フォームの作成 通信はPOST通信で-->
-<h2>①ネットワークのポート番号は何番？</h2>
 
-<h2>②Webページを作成するための言語は？</h2>
-<!--③ 問題のradioボタンを「foreach」を使って作成する-->
+<p>お疲れ様です<?php echo $my_name;?>さん</p>
 
-<h2>③MySQLで情報を取得するためのコマンドは？</h2>
-<!--③ 問題のradioボタンを「foreach」を使って作成する-->
+<h2>①ネットワークのポート番号は何番？
+$number = [80 , 22 , 20 , 21];
+forreach($number as $value) {
+    echo $value;
+}
+</h2>
 
-<!--問題の正解の変数と名前の変数を[answer.php]に送る-->
+<h2>②Webページを作成するための言語は？
+$language = ["PHP" , "Python" , "JAVA" , "HTML"];
+forreach($language as  value) {
+    echo $value;
+}
+</h2>
+
+<h2>③MySQLで情報を取得するためのコマンドは？
+$command = ["join" , "select" , "insert" , "update"];
+forreach($command as $value) {
+    echo $value;
+}
+</h2>
+
+<input type="submit" value="同意する"/>
+
+
