a15bc88de3f3ce8d991d05fec502174ca9d38bea
Leotamn <103520627+Leotamn@users.noreply.github.com>
2022-09-27 00:11:36 +0900

    修正
    fortune,resultともに全体的に修正

--- PHPjob/3-3/result.php
+++ PHPjob/3-3/result.php
@@ -6,8 +6,56 @@ $number = $POST['number'];
 
 <p><?php echo date("Y-m-d"); ?>の運勢は</p>
 <p>選ばれた数字は<?php echo $number; ?></p>
+
 <?php
-$str_split
+$str_split($string [,$split_length = 1])
+
+$lots = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+if (isset($_POST['number'])) {
+    $lot = array_rand($lots);
+    switch ($lot) {
+        case 0;
+           $res = '凶';
+           $num = 0;
+           break;  
+        case 1;
+           $res = '小吉';
+           $num = 1;
+           break;   
+        case 2;
+           $res = '小吉';
+           $num = 2;
+           break;   
+        case 3;
+           $res = '小吉';
+           $num = 3;
+           break;   
+        case 4;
+           $res = '中吉';
+           $num = 4;
+           break;   
+        case 5;
+           $res = '中吉';
+           $num = 5;
+           break;
+        case 6;
+           $res = '中吉';
+           $num = 6;
+           break;
+        case 7;
+           $res = '吉';
+           $num = 7;
+           break;  
+        case 8;
+           $res = '吉';
+           $num = 8;
+           break; 
+        case 9;
+           $res = '吉';
+           $num = 9;
+           break; 
+    }
+
 
 ?>
 
