b713dd6d3b8b059d6177d73dc5e845fde95b4d77
Leotamn <103520627+Leotamn@users.noreply.github.com>
2022-11-23 23:29:19 +0900

    あ
    修正

--- PHPjob/3-4/question.php
+++ PHPjob/3-4/question.php
@@ -1,36 +1,52 @@
+<form action = "answer.php" method = "POST">
+
 <?php
  
- $my_name = array();
- $number = array();
- $language = array();
- $command = array();
+ $my_name =$_POST['my_name'];
+ $number = $_POST['number'];
+ $language = $_POST['language'];
+ $command = $_POST['command'];
  
 
+
+<p>お疲れ様です<?php echo $my_name ?>さん</p>
+
+<h2>①ネットワークのポート番号は何番？</h2>
+<?php
+$number = [80 , 22 , 20 , 21];
+
+foreach($number as $value) {  ?>
+    <input type = "radio">
+    <?php echo $value;
+}
 ?>
 
-<p>お疲れ様です<?php echo $my_name;?>さん</p>
 
-<h2>①ネットワークのポート番号は何番？
-$number = [80 , 22 , 20 , 21];
-forreach($number as $value) {
-    echo $value;
-}
-</h2>
-
-<h2>②Webページを作成するための言語は？
+<h2>②Webページを作成するための言語は？</h2>
+<?php
 $language = ["PHP" , "Python" , "JAVA" , "HTML"];
-forreach($language as  value) {
-    echo $value;
-}
-</h2>
 
-<h2>③MySQLで情報を取得するためのコマンドは？
-$command = ["join" , "select" , "insert" , "update"];
-forreach($command as $value) {
-    echo $value;
+foreach($language as $value) { ?>
+    <input type = "radio">
+    <?php echo $value;
 }
-</h2>
+?>
+
+
+<h2>③MySQLで情報を取得するためのコマンドは？</h2>
+<?php
+$command = ["join" , "select" , "insert" , "update"];
+
+foreach($command as $value) { ?>
+    <input type = "radio">
+    <?php echo $value;
+}
+?>
+<br>
+
 
 <input type="submit" value="同意する"/>
 
+</form>
 
+?>
