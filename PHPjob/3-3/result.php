17cec08601dced2a36fe2a42ad9006f3878f721d
Leotamn <103520627+Leotamn@users.noreply.github.com>
2022-11-30 00:01:32 +0900

    lot
    削除

--- PHPjob/3-3/result.php
+++ PHPjob/3-3/result.php
@@ -9,8 +9,8 @@ $num = $array[$keys];
 
 $lots = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 if (isset($_POST['number'])) {
-    $lot = array_rand($lots);
-    switch ($lot) {
+   
+    switch ($num) {
         case 0;
            $res = '凶';
            $num = 0;
